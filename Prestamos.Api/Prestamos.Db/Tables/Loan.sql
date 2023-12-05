915b39179dfdd25a8a65d4cdcea6126f63c67d36
Juan Ignacio Pucheta <80986033+JuaniPucheta@users.noreply.github.com>
2023-12-05 17:04:43 -0300

    Prestamos.Api y Prestamos.Db

---
+++ Prestamos.Api/Prestamos.Db/Tables/Loan.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Loan]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Date] DATETIME NOT NULL, 
+    [ReturnDate] DATETIME NULL, 
+    [Status] NCHAR(10) NOT NULL,
+    [PersonID] INT NOT NULL,
+    [ThingID] INT NOT NULL, 
+)
+
+GO
+    ALTER TABLE [dbo].[Loan] ADD CONSTRAINT [FK_Loan_Person_PersonID] FOREIGN KEY([PersonID])
+    REFERENCES [dbo].[Person] ([Id])
+GO
+GO
+    ALTER TABLE [dbo].[Loan] ADD CONSTRAINT [FK_Loan_Thing_ThingID] FOREIGN KEY([ThingID])
+    REFERENCES [dbo].[Thing] ([Id])
+GO
---
+++ Prestamos.Api/Prestamos.Db/Tables/Thing.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Thing]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Description] VARCHAR(50) NOT NULL, 
+    [CreationDate] DATETIME NOT NULL,
+    [CategoryID] INT NOT NULL
+)
+GO
+    ALTER TABLE [dbo].[Thing] ADD CONSTRAINT [FK_Thing_CategoryID] FOREIGN KEY([CategoryID])
+    REFERENCES [dbo].[Category] ([Id])
+GO
